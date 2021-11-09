d21b1aeae45081eaf910ae3c1b42f9159c8de905
WeiErdong <71922013+Will8296535@users.noreply.github.com>
2021-11-09 11:56:04 +0800

    Add files via upload

---
+++ chat system based on C#/Assignment05/MainWindow.xaml.cs
@@ -0,0 +1,161 @@
+﻿/*
+ FILE			:MainWindw.xaml.cs
+ PROJECT        :PROG2121-20F Assignment5
+ PRPGRAMMER     :Will Wei
+ FIRST VERSION  :2020-11-01
+ DESCRIPTION	:This file contains the client of a chat system 
+                 that using TCP/IP to communicate 
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Net.Sockets;
+using System.Threading;
+using System.Net;
+
+namespace Assignment05
+{
+    public partial class MainWindow : Window
+    {
+        private TcpClient client;
+        private NetworkStream stream;
+        private string readData = null;
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        //
+        // FUNCTION : Connect_Click
+        // DESCRIPTION :
+        // This function create a new client to connect to the server
+        // and start a thread to send/receive message from server.
+        // PARAMETERS :
+        // None
+        // RETURNS :
+        // none
+        //
+        private void Connect_Click(object sender, RoutedEventArgs e)
+        {
+            readData = "Connected to Server...";
+            msg();
+
+            //client = new TcpClient("127.0.0.1", 13000);
+            IPHostEntry ipHostInfo = Dns.GetHostEntry(Dns.GetHostName());
+            IPAddress ipAddress = ipHostInfo.AddressList[3];
+            client = new TcpClient(ipAddress.ToString(), 13000);
+
+            stream = client.GetStream();
+            Thread myThread = new Thread(getMessage);
+            myThread.Start();
+            SendButton.IsEnabled = true;
+            ConnectButton.IsEnabled = false;
+            message.IsEnabled = true;
+            
+        }
+        //
+        // FUNCTION : getMessage
+        // DESCRIPTION :
+        // This function will receive the message from server through the stream
+        // and show it to the TextBlock
+        // If the message is "0", then close the stream and client
+        // PARAMETERS :
+        // None
+        // RETURNS :
+        // none
+        //
+        private void getMessage()
+        {
+            while(true)
+            {
+                try
+                {
+                    
+                    byte[] data = new byte[256];
+                    stream.Read(data, 0, data.Length);
+                    readData = System.Text.Encoding.ASCII.GetString(data);
+                    // When recieve '0', disconnect from server
+                    if(readData[0] == '0')
+                    {
+                        readData = "Disconnect";
+                        msg();
+                        stream.Close();
+                        client.Close();
+                        break;
+                    }
+                    else
+                    {
+                        msg();
+                    }
+
+                }
+                catch(SocketException e)
+                {
+                    throw new System.ArgumentException("Socket Exception.", e);
+                }
+                catch(System.IO.IOException e)
+                {
+                    throw new System.ArgumentException("IO Exception.", e);
+                }
+            }
+           
+            
+        }
+        //
+        // FUNCTION : Button_Clic
+        // DESCRIPTION :
+        // This function will get the message from massage box 
+        // and send it to the server through the stream
+        // PARAMETERS :
+        // None
+        // RETURNS :
+        // none
+        //
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            
+            // Translate the passed message into ASCII and store it as a Byte array
+            Byte[] data = System.Text.Encoding.ASCII.GetBytes(message.Text);
+
+            // Send the message to the connected TcpServer.
+            stream.Write(data, 0, data.Length);
+            stream.Flush();
+
+            // Empty the massage box
+            message.Text = "";
+            
+        }
+        //
+        // FUNCTION : msg
+        // DESCRIPTION :
+        // This function will update the text in ReceiveTxt TextBlock
+        // PARAMETERS :
+        // None
+        // RETURNS :
+        // none
+        //
+        private void msg()
+        {
+            if(ReceiveTxt.Dispatcher.Thread == Thread.CurrentThread)
+            {
+                ReceiveTxt.Text = ReceiveTxt.Text + "\n" + readData;
+            }
+            else
+            {
+                ReceiveTxt.Dispatcher.Invoke(new Action(msg));
+            }
+        }
+    }
+}
---
+++ chat system based on C#/TCPIPServer/Program.cs
@@ -0,0 +1,142 @@
+﻿/*
+ FILE			:Program.cs
+ PROJECT        :PROG2121-20F Assignment5
+ PRPGRAMMER     :Will Wei
+ FIRST VERSION  :2020-11-01
+ DESCRIPTION	:This file contains the server of a chat system 
+                 that using TCP/IP to communicate 
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+
+namespace TCPIPServer
+{
+    
+    class Program
+    {
+        static volatile bool Run = true;
+        static void Main(string[] args)
+        {
+            List<TcpClient> clients = new List<TcpClient>();
+            TcpListener server = null;
+            try
+            {
+                // Set the TcpListener on port 13000 and listening for client requests.
+               
+                IPHostEntry ipHostInfo = Dns.GetHostEntry(Dns.GetHostName());
+                IPAddress ipAddress = ipHostInfo.AddressList[3];
+                server = new TcpListener(ipAddress, 13000);
+
+                //Int32 port = 13000;
+                //IPAddress localAddr = IPAddress.Parse("127.0.0.1");
+                //server = new TcpListener(localAddr, port);
+                server.Start();
+
+                // Enter the listening loop.
+                while (true)
+                {
+                    Console.Write("Waiting for a connection... ");
+
+                    // Perform a blocking call to accept requests.
+                    // When server receive a request from client, it add the client to clients list
+                    // and start a new thread for communication 
+                    TcpClient client = server.AcceptTcpClient();
+                    clients.Add(client); 
+                    data clientData = new data() {
+                        cs = clients,
+                        c = client
+                    };
+                    Console.WriteLine("Connected!");
+                    ParameterizedThreadStart ts = new ParameterizedThreadStart(Worker);
+                    Thread clientThread = new Thread(ts);
+                    clientThread.Start(clientData);
+
+                }
+            }
+            catch (SocketException e)
+            {
+                throw new System.ArgumentException("Socket Exception.", e);
+            }
+            finally
+            {
+                // Stop listening for new clients.
+                server.Stop();
+            }
+        }
+        struct data
+        {
+            public List<TcpClient> cs;
+            public TcpClient c;
+        }
+
+        //
+        // FUNCTION : Worker
+        // DESCRIPTION :
+        // 
+        // PARAMETERS :
+        // object: o contains the client 
+        // and the List of all clients that connected to the server
+        // RETURNS :
+        // none
+        //
+        public static void Worker(Object o)
+        {
+            data clientData = (data)o;
+            TcpClient client = clientData.c;
+            // Buffer for reading data
+            byte[] bytes = new byte[256];
+            String data = null;
+            NetworkStream stream = client.GetStream();
+            // Loop to receive all the data sent by the client.
+            while (Run)
+            {
+                try
+                {
+                    // Receive the data from client
+                    int i = stream.Read(bytes, 0, bytes.Length);
+                    data = System.Text.Encoding.ASCII.GetString(bytes, 0, i);
+                    Console.WriteLine("Received: {0}", data);
+                    byte[] msg = System.Text.Encoding.ASCII.GetBytes(data);
+                    if (data == "0")
+                    {
+                        // when receive "0" from client, close the thread
+                        Run = false;
+                        stream.Write(msg, 0, msg.Length);
+                        stream.Flush();
+                        Console.WriteLine("Sent: {0}", data);
+                        // delete the handle from the list
+                        clientData.cs.Remove(client);
+                    }
+                    // Send back a response to every client
+                    else
+                    {
+                        foreach (TcpClient c in clientData.cs)
+                        {
+                            NetworkStream s = c.GetStream();
+                            s.Write(msg, 0, msg.Length);
+                            s.Flush();
+                            Console.WriteLine("Sent: {0}", data);
+                        }
+                    }
+
+
+                }
+                catch(Exception ex)
+                {
+                    throw new System.ArgumentException("Error", ex);
+                }
+                
+            }
+
+            
+        }
+        
+    }   
+}
